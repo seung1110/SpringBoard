548be9ec225675aa285b088c91b2730b82987da1
Seungyub <iiwvii@naver.com>
2021-05-11 16:35:36 +0900

    기본 crud 구현

--- SpringStudy/src/main/java/org/psy/mapper/BoardMapper.java
+++ SpringStudy/src/main/java/org/psy/mapper/BoardMapper.java
@@ -7,4 +7,7 @@ import org.psy.domain.BoardVO;
 public interface BoardMapper {
 	public BoardVO getBoard(int bno);
 	public List<BoardVO> getBoardList();
+	public int insertBoard(BoardVO vo);
+	public int deletBoard(int bno);
+	public int updateBoard(BoardVO vo);
 }
--- SpringStudy/src/test/java/org/psy/db/MapperTest.java
+++ SpringStudy/src/test/java/org/psy/db/MapperTest.java
@@ -2,6 +2,7 @@ package org.psy.db;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.psy.domain.BoardVO;
 import org.psy.mapper.BoardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -17,8 +18,34 @@ public class MapperTest {
 	@Setter(onMethod_=@Autowired)
 	private BoardMapper mapper;
 	
+//	@Test
+//	public void BoardMapperTest(){
+//		log.info(mapper.getBoard(11));
+//	}
+	
+//	@Test
+//	public void insertTest(){
+//		BoardVO vo = new BoardVO();
+//		vo.setContent("새로운 내용2");
+//		vo.setWriter("테스터1");
+//		vo.setTitle("insert test2");
+//		mapper.insertBoard(vo);
+//		log.info(vo);
+//	}
+	
+//	@Test
+//	public void deletTest(){
+//		log.info(mapper.deletBoard(11));
+//	}
+	
 	@Test
-	public void BoardMapperTest(){
-		log.info(mapper.getBoard(1));
+	public void updateTest(){
+		BoardVO vo = new BoardVO();
+		vo.setBno(10);
+		vo.setContent("수정된 내용");
+		vo.setTitle("update test");
+		
+		log.info(mapper.updateBoard(vo));
 	}
+	
 }
